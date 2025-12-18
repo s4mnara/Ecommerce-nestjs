888ea5925995b24356999d39b3f521203a3b665e
Sam <samaraalmeidaah@gmail.com>
2025-12-18 19:11:55 -0300

    todos os comits com ponto são para testes de deploy!

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
-import { toast } from "react-toastify"; // import do toast
+import { toast } from "react-toastify"; 
 import 'react-toastify/dist/ReactToastify.css';
 
 function LoginPage({ onLoginSuccess }) {
